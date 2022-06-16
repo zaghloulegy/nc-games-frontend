491191610283a69ff2edfd3c0e7a4e6958c50aa2
zaghloulegy <zaghloulegy@gmail.com>
2022-06-16 11:50:58 +0100

    working on login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { React, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { getFirstName } from "../utils/utilFuncs";
-import SearchBox from "./SearchBox";
+import SearchBox from "./sorting";
 import northercoders from "../img/northercoders.png";
 
 export default function Header() {
